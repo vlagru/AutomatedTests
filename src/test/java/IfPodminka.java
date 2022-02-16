dd21c00afbd72e1dd24ac079300fe27685d004b0
vlagru <v.grulich@seznam.cz>
2022-02-16 18:28:11 +0100

    Add new test

---
+++ src/test/java/IfPodminka.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IfPodminka {
+    private WebDriver driver;
+    private final String BASE_URL = "http://localhost/clickmebaby.php";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void test(){
+        driver.get(BASE_URL);
+        Assert.assertEquals("Pocatecni pocet kliku", "0", driver.findElement(By.id("clicks")).getText());
+        Assert.assertEquals("klikov", driver.findElement(By.className("description")).getText());
+
+        for (int i = 1; i < 11; i++) {
+            driver.findElement(By.id("clickMe")).click();
+            Assert.assertEquals(String.valueOf(i), driver.findElement(By.id("clicks")).getText());
+                if (i==1) {
+                    Assert.assertEquals("klik", driver.findElement(By.className("description")).getText());
+                }
+        }
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
